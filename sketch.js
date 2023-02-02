f685a3b648fa4f01e5c5bb5136b774fa8e67f9cb
MiguelMao08 <109632976+MiguelMao08@users.noreply.github.com>
2023-02-02 19:23:09 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,35 @@
-var heroi, inimigo;
-var imgFundo, imgHeroi, imgInimigo;
+var heroi, caveira, maca, arvore, estrela;
+var imgFundo, imgHeroi, imgCaveira, imgMaca, imgArvore, ImgEstrela;
 var soloInvisivel;
+var groupCaveira, groupArvore;
 
 function preload(){
-  imgFundo=loadImage("imagens/imagem_de_fundo_inicio.jpg")
+  imgFundo=loadImage("imagens/imagem_de_fundo_inicio.jpg");
+  imgHeroi=loadImage("imagens/heroi.png");
+  imgCaveira=loadImage("imagens/caveira.png");
+  imgMaca=loadImage("imagens/maca.png");
+  imgArvore=loadImage("imagens/arvore.png");
+  ImgEstrela=loadImage("imagens/estrela.png");
 
 }
 
 function setup(){
 createCanvas(windowWidth, windowHeight);
+
+//sprite heroi
 heroi=createSprite(width/2-500,height/2);
-inimigo=createSprite(width/2+500,height/2);
+heroi.addImage("heroi", imgHeroi);
+heroi.scale=0.05;
 
 //solo invisivel
-soloInvisivel = createSprite(width/2-1000,height-135,2550,20);
+soloInvisivel = createSprite(width/2-1000,height-135,100000,20);
+
+//velocidade caveira
+caveira
+
+//grupos
+groupCaveira=new Group();
+groupArvore=new Group();;
 }
 
 function draw(){
@@ -28,8 +44,13 @@ function draw(){
     heroi.x-=20;
   }
 
-  if(keyDown("space") && heroi.y>=551){
-    heroi.y-=35
+  if(keyDown("space") && heroi.y>=543){
+    heroi.y-=100
+  }
+
+  //movimento da caveira
+  if(caveira<1100){
+    caveira.velocityX+=0.5
   }
   console.log(heroi.y);
 
@@ -37,11 +58,38 @@ function draw(){
   camera.x = heroi.x;
 
   //gravidade
-  heroi.velocityY+=2;
+  heroi.velocityY+=1;
+  groupCaveira.setVelocityYEach(3);
+  //groupArvore.setVelocityXEach(-5);
 
   //colisao
   heroi.collide(soloInvisivel);
+  groupCaveira.collide(soloInvisivel);
+
+  //chamar funçao
+  criarArvore();
+  criarCaveira();
 
   drawSprites();
+}
+
+function criarArvore(){
+  if(frameCount%100===0){
+    arvore=createSprite(Math.round(random(width/2+460, width*30)),height/2+90);
+  arvore.addImage("arvore", imgArvore);
+  arvore.scale=0.09;
+  groupArvore.add(arvore);
+  }
+}
+
+
+function criarCaveira(){
+  if(frameCount%100===0){
+    caveira=createSprite(Math.round(random(width/2+500, width*30)),height/2);
+  caveira.addImage("caveira", imgCaveira);
+  caveira.scale=0.05;
+  groupCaveira.add(caveira);
+  heroi.depth=arvore.depth+1;
+  }
 }
 
